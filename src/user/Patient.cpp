7ab0f707825a7b344d0d368ecd7c740faa2f220e
Canuc80k <Canuc80k@gmail.com>
2023-11-27 10:54:20 +0700

    Add Patient object

---
+++ src/user/Patient.cpp
@@ -0,0 +1,75 @@
+#include "Patient.h"
+#include "../calendar/Calendar.cpp"
+#include "../drug/Drug.h"
+#include "../management/DrugManagement.cpp"
+#include "../management/ReceiptManagement.cpp"
+#include "../management/AppointmentManagement.cpp"
+#include <iostream>
+using namespace std;
+
+Patient::Patient() : User(0, "", "", "") {
+    this->rec = new LinkedList<string>*;
+    this->cart = Cart();
+}
+
+Patient::Patient(int id, string name, string email, string password, LinkedList<string> rec) : User(id, name, email, password) {
+    this->rec = rec;
+    this->cart = Cart();
+}
+
+Patient::~Patient() {
+    delete rec;
+    delete cart;
+}
+
+Cart &Patient::getCart() {return this->cart;}
+LinkedList<string>* Patient::getRec() {return this->rec;}
+void Patient::setRec(LinkedList<string>* rec) {this->rec = rec;}
+
+void Patient::purchase() {
+    // string recID = "REC#" + to_string(this->rec.size() + 1);
+    // this->rec.addLast(recID);
+
+    // string fileName = to_string(this->getID()) + "_" + recID + ".txt";
+    // string filePath = "../../Database/ReceiptDB/" + fileName;
+    // ofstream outFilePatient("../../Database/UserDB/PatientDB/PATIENT_" + to_string(this->getID()) + ".txt", ios::app);
+    // outFilePatient << recID << '\n';
+    // outFilePatient.close();
+
+    // ofstream outFile(filePath);
+    // string date = currentDateTime();
+    // outFile << date << '\n';
+    // outFile << cart.getTotalPrice() << '\n';
+
+    // for (int i = 0; i < (*cart.getDrugIDList()).length(); i++) {
+    //     outFile << (*cart.getDrugQuantityList()).get(i) << " ";
+    //     outFile << (*cart.getDrugIDList()).get(i) << '\n';
+    // }
+
+    // outFile.close();
+    // printReceipt(this->getID(), recID);
+
+    // _Cart.emptyCart();
+}
+
+void Patient::searchByReceiptID(int id) {
+    getReceiptFromDatabase(this->getID(), id);
+}
+
+void Patient::printALLPatientReceipts() {
+    getAllPatientReceipts(*this);
+}
+
+void Patient::addNewReceipt() {
+    addReceiptToDatabase(this->getID(), this->cart, this->rec);
+}
+
+void Patient::deleteReceiptById(string id) {
+    deleteReceiptFromDatabase(this->getID(), id);
+}
+
+void Patient::bookAnAppointment() {
+}
+
+void Patient::printMedicalHistory() {
+}
---
+++ src/user/Patient.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "User.cpp"
+#include "Cart.cpp"
+#include "../datastructure/LinkedList.cpp"
+
+class Patient : public User {
+    private:
+        Cart cart;
+        LinkedList<string>* rec;
+
+    public:
+        Patient();
+        Patient(int id, string name, string email, string password, LinkedList<string> revs)
+        ~Patient();
+
+        Cart &getCart();
+        LinkedList<string>* getRec();
+        void setRec(LinkedList<string>*);
+        void purchase();
+        void searchByReceiptID(int);
+        void printALLPatientReceipts();
+        void addNewReceipt();
+        void deleteReceiptById(string);
+        void bookAnAppointment();
+        void printMedicalHistory();
+};
